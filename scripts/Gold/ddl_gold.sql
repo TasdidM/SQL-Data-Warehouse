46b3b0a3fc7da4bec8cf4871b66c34657978be6b
TasdidM <tasdid.mahmud01@gmail.com>
2026-01-01 21:49:01 +0100

    Create Gold level views in data warehouse
    This script creates views for the Gold level in the data warehouse, including dimension tables for customers and products, as well as a fact table for sales.

---
+++ scripts/Gold/ddl_gold.sql
@@ -0,0 +1,101 @@
+/*
+================================================================================
+DDL Script: Creare Gold View
+================================================================================
+Scopo dello script:
+    Questo script crea viste per il livello Gold nel data warehouse.
+    Il livello Gold rappresenta le tabelle delle dimensioni e fatti 
+    finali (Star Schema)
+
+    Ogni viste esegue trasformazioni e combina i dati provenienti dal
+    livello Silver per produrre un set dei dati puliti, arricchiti e pronti
+    per l'uso aziendale.
+
+Utilizzo:
+    - Queste View possono essere utilizzate direttamente per analisi e
+    reporting.
+================================================================================
+*/
+
+
+-- =============================================================================
+-- Creare tabella delle dimensioni: gold.dim_customers
+-- =============================================================================
+IF OBJECT_ID('gold.dim_customers', 'V') IS NOT NULL
+    DROP VIEW gold.dim_customers;
+GO
+
+CREATE VIEW gold.dim_customers AS
+Select
+    ROW_NUMBER() OVER (ORDER BY cst_id) AS customer_key,
+    ci.cst_id AS customer_id,
+    ci.cst_key AS customer_number,
+    ci.cst_firstname AS first_name,
+    ci.cst_lastname AS last_name,
+    la.cntry AS country,
+    ci.cst_marital_status AS marital_status,
+    CASE
+        -- CRM è la fonte primaria per il genere
+        WHEN ci.cst_gndr != 'Unknown' THEN ci.cst_gndr
+        -- Ritorno ai dati ERP
+        ELSE COALESCE(ca.gen, 'Unknown')
+    END AS gender,
+    ca.bdate AS birth_date,
+    ci.cst_create_date AS create_date
+FROM silver.crm_cust_info ci
+LEFT JOIN silver.erp_cust_az12 ca
+ON ci.cst_key = ca.cid
+LEFT JOIN silver.erp_loc_a101 la
+ON ci.cst_key = la.cid;
+GO
+
+-- =============================================================================
+-- Creare tabella delle dimensioni: gold.dim_products
+-- =============================================================================
+IF OBJECT_ID('gold.dim_products', 'V') IS NOT NULL
+    DROP VIEW gold.dim_products;
+GO
+
+CREATE VIEW gold.dim_products AS
+SELECT
+    ROW_NUMBER() OVER (ORDER BY pn.prd_start_dt, pn.prd_key) AS product_key,
+    pn.prd_id AS product_id,
+    pn.prd_key AS product_number,
+    pn.prd_nm AS product_name,
+    pn.cat_id AS category_id,
+    pc.cat AS category,
+    pc.subcat AS cubcategory,
+    pc.maintenance,
+    pn.prd_cost AS cost,
+    pn.prd_line AS product_line,
+    pn.prd_start_dt AS start_date
+FROM silver.crm_prd_info pn
+LEFT JOIN silver.erp_px_cat_g1v2 pc
+ON pn.cat_id = pc.id
+-- filtrare tutti i dati storici
+WHERE pn.prd_end_dt IS NULL;
+GO
+
+-- =============================================================================
+-- Creare tabella dei fatti: gold.fact_sales
+-- =============================================================================
+IF OBJECT_ID('gold.fact_sales', 'V') IS NOT NULL
+    DROP VIEW gold.fact_sales;
+GO
+
+CREATE VIEW gold.fact_sales AS
+SELECT
+    sd.sls_ord_num AS order_number,
+    pr.product_key,
+    cu.customer_key,
+    sd.sls_order_dt AS order_date,
+    sd.sls_ship_dt AS shipping_date,
+    sd.sls_due_dt AS due_date,
+    sd.sls_sales AS sales_amount,
+    sd.sls_quantity AS quantity,
+    sd.sls_price AS sales_price
+FROM silver.crm_sales_details sd
+LEFT JOIN gold.dim_products pr
+ON sd.sls_prd_key = pr.product_number
+LEFT JOIN gold.dim_customers cu
+ON sd.sls_cust_id = cu.customer_id;
